af88d5f1a52487023b29f74bc3ad0ef1de093e95
botuhok <vonavivl@gmail.com>
2024-04-18 23:17:33 +0300

    configs

--- i3/feh_timer.sync-conflict-20240418-203701-KSGTDJ3.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-while true; do
-  feh --randomize --bg-fill ~/Sync/wall
-  sleep 300
-done
--- i3/feh_timer.sync-conflict-20240418-224332-KSGTDJ3.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-while true; do
-  feh --randomize --bg-fill ~/wall
-  sleep 300
-done
